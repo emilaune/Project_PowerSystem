fa089d9945a290ef791b4bc01dc66376cb653f1f
Emil Aune Jakobsen <emilajakobsen@gmail.com>
2021-11-03 10:29:30 +0100

    la inn en print

--- project_partA.py
+++ project_partA.py
@@ -53,3 +53,4 @@ if __name__ == '__main__':
     busmatrix = getbusdata("/Users/emiljakobsen/Documents/NTNU 4.klasse/7. semester/Power System Analysis/Project/project_busdata.xlsx")
     buses = getbuses("/Users/emiljakobsen/Documents/NTNU 4.klasse/7. semester/Power System Analysis/Project/project_busdata.xlsx")
     print(busmatrix)
+    print("hei")
