4f96dc5b89b27e88250ec849b9f834800348477d
Emil Aune Jakobsen <emilajakobsen@gmail.com>
2021-11-03 11:26:02 +0100

    vet ikke

--- project_partA.py
+++ project_partA.py
@@ -49,7 +49,7 @@ def getbuses(filepath):  # takes the filepath as input parameter and uses getbus
 
 
 if __name__ == '__main__':
-    busmatrix = getbusdata("/Users/emiljakobsen/Documents/NTNU 4.klasse/7. semester/Power System Analysis/Project/project_busdata.xlsx")
+    busmatrix = getbusdata("/Users/emiljakobsen/Documents/NTNU 4.klasse/7. semester/Power System Analysis/Project/Project_PowerSystem/project_busdata.xlsx")
     buses = getbuses("/Users/emiljakobsen/Documents/NTNU 4.klasse/7. semester/Power System Analysis/Project/project_busdata.xlsx")
     print(busmatrix)
     print("heipåeskil")
